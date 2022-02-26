cf4666c2fcc46dd37039a89e88fca2e97a631d0c
Fernando Aguilar <feraguilar6985@gmail.com>
2022-02-26 14:27:20 -0600

    feat(beneficiaries):add beneficiaries endpoints

---
+++ src/pages/api/beneficiaries/[id].ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import jwt from 'jsonwebtoken'
+import connection from '../../../utils/database'
+
+export default async(req: NextApiRequest, res: NextApiResponse) => {
+  const {method, body, query: {id}} = req
+
+  switch(method) {
+    case 'PUT':
+      try {
+        const token = req.headers.authorization
+        if (!token) {
+          return res.status(401).json({message: 'Not autenticated'})
+        }
+        const payload = jwt.verify(token, process.env.SECRET as string)
+        const [rows, fields] = await connection.query('UPDATE beneficiaries SET fullname = ?, hampers = ?, age =?, gender = ? WHERE id_beneficiary = ?',
+        [body.fullname, body.hampers, body.age, body.gender, id])
+        return res.status(200).json({message: 'Hampers successfully updated', rows})
+      } catch (error) {
+        return res.status(400).json({message: error}) 
+      }
+    case 'GET':
+      try {
+        const token = req.headers.authorization
+        if (!token) {
+          return res.status(401).json({message: 'Not autenticated'})
+        }
+        const [rows, fields] = await connection.execute('SELECT * FROM beneficiaries WHERE id_beneficiary = ?', [id])
+        return res.status(200).json({message: 'Get one beneficiarie', rows})
+      } catch (error) {
+        return res.status(400).json({message: error}) 
+      }
+      
+  }
+}
---
+++ src/pages/api/beneficiaries/index.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import jwt from 'jsonwebtoken'
+import connection from '../../../utils/database'
+
+export default async(req: NextApiRequest, res: NextApiResponse) => {
+  const {method, body} = req
+
+
+  switch(method) {
+    case 'POST':
+      try {
+        const token = req.headers.authorization
+        if (!token) {
+          return res.status(401).json({message: 'Not autenticated'})
+        }
+        const payload = jwt.verify(token, process.env.SECRET as string)
+        
+        if(body.age < 18){
+          return res.status(400).json({message: 'You have to be over 18 years old'})
+        }
+
+        const [rows, fields] = await connection.query
+        ('INSERT INTO beneficiaries(fullname, hampers, id_user, age, gender) VALUES (?,?,?,?,?)',
+        [body.fullname, body.hampers, payload.sub, body.age, body.gender])
+        return res.status(200).json({message: 'Hampers successfully inserted', rows})
+      } catch (error) {
+        return res.status(400).json({message: error}) 
+      }
+    case 'GET': {
+      try {
+        const token = req.headers.authorization
+        if (!token) {
+          return res.status(401).json({message: 'Not autenticated'})
+        }
+        const payload = jwt.verify(token, process.env.SECRET as string)
+
+        const [rows, fields] = await connection.execute('SELECT * FROM beneficiaries')
+        return res.status(200).json({message: 'Beneficiaries list', rows})
+      } catch (error) {
+        return res.status(400).json({message: error}) 
+      }
+    }
+  }
+}
